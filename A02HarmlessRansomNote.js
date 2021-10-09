a65ffe7869c8b20e8246bc0dea95f783d75d3a10
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-10-09 05:21:57 +0200

    update

--- A02HarmlessRansomNote.js
+++ A02HarmlessRansomNote.js
@@ -1,13 +1,34 @@
 function harmlessRansomNote(noteText, magazineText) {
-  let magazineWords = magazineText.split(" ");
-  for (let noteWord of noteText) {
-    if (magazineWords.includes(noteWord)) {
-      return true;
+  let noteArray = noteText.split(" ");
+  let magazineArray = magazineText.split(" ");
+  let magazineObj = {};
+
+  magazineArray.forEach((word) => {
+    if (!magazineObj[word]) {
+      magazineObj[word] = 0;
     }
-  }
+    magazineObj[word]++;
+  });
+
+  let noteIsPossible = true;
+  noteArray.forEach((word) => {
+    if (magazineObj[word]) {
+      magazineObj[word]--;
+      if (magazineObj[word] < 0) {
+        noteIsPossible = false;
+      }
+    } else {
+      noteIsPossible = false;
+    }
+  });
+
+  return noteIsPossible;
 }
 
 harmlessRansomNote(
-  "my job is to teach you something",
-  "your job is to learn something"
+  "this is a secret note for you from a secret admirer",
+  "puerto rico is a place of great wonder and excitement it has many secret waterfall locations that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited"
 );
+
+// Linear Time Complexity or O (n) run time - two loops, but they are not nested
+// O (n) + O (m) => O (n + m) => O (n)
---
+++ A03IsPalindrome.js
@@ -0,0 +1,43 @@
+// My Solution
+function isPalindrome(string) {
+  let newString = "";
+
+  for (let i = 0; i < string.length; i++) {
+    if (
+      (string.codePointAt(i) >= 65 && string.codePointAt(i) <= 90) ||
+      (string.codePointAt(i) >= 97 && string.codePointAt(i) <= 122)
+    ) {
+      newString += string[i];
+    }
+  }
+
+  let reversedString = newString.split("").reverse().join("");
+
+  let trueOrFalse =
+    newString.toLowerCase() == reversedString.toLowerCase() ? true : false;
+
+  return trueOrFalse;
+}
+
+isPalindrome("iv*&anmile$%nko978vic");
+
+function isPalindrome(string) {
+  string = string.toLowerCase();
+  let charactersArray = string.split("");
+  let validCharacters = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  let lettersArray = [];
+  charactersArray.forEach((char) => {
+    if (validCharacters.indexOf(char) > -1) {
+      lettersArray.push(char);
+    }
+  });
+
+  if (lettersArray.join("") === lettersArray.reverse().join("")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isPalindrome("Madam, I'm Adam");
